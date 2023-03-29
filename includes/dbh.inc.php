c5aeb038be483f2b50717951e9d41381d9772b78
RajaThanigainayagam <thanigainayagam@yahoo.fr>
2023-03-29 10:52:59 +0200

    error corrected in update.menu.inc.php at 29 10.52

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,11 +1,13 @@
 <?php
 
-$servername = "localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "quaiantique";
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = mysqli_connect($server, $username, $password, $db);
 
 if (!$conn){
     die("Connection failed:" .mysqli_connect_error());
